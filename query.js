4dd062f15accf44a77355fad9a4a5789c0b0833d
A J <aryanthejain@gmail.com>
2025-02-08 22:45:12 +0000

    Encryption baeeeeee

--- query.js
+++ query.js
@@ -1,5 +1,6 @@
 import Gun from 'gun';
 import * as readline from 'readline';
+import SEA from 'gun/sea.js';
 
 const gun = Gun(['http://localhost:3001/gun', 'http://localhost:3002/gun', 'http://localhost:3003/gun']);
 
@@ -9,12 +10,13 @@ class Query {
         this.user = gunInstance.get(this.db);
     }
 
-    add(key, data) {
-        this.user.get(key).put(data)
-        console.log(`Added data to '${this.db}/${key}':`, data);
+    async add(key, data) {
+        let hidden = await SEA.encrypt(data, this.db);
+        this.user.get(key).put(hidden)
+        console.log(`Added data to '${this.db}/${key}':`, hidden);
     }
 
-    update(key, data) {
+    update(key, data) {     
         this.user.get(key).put(data)
         console.log(`Updated data to '${this.db}/${key}':`, data);
     }
@@ -26,11 +28,12 @@ class Query {
     }
 
     async read(key) {
-        const val = await this.user.get(key)
+        const encryptedObject = await this.user.get(key)
+        const decrypted = await SEA.decrypt(encryptedObject, this.db);
         if (val == undefined) {
             return "Error: Not a well formed expression"
         }
-        return val
+        return decrypted
     }
 
     async readAll() {
@@ -69,7 +72,7 @@ async function queryLang(user) {
                     let value = JSON.parse(result[2]);
                     const alreadyExists = await db.read(key)
                     if (alreadyExists == undefined) {
-                        db.add(key, value)
+                        await db.add(key, value)
                     } else {
                         returnstring = "Error: Not a well formed expression"
                     }
@@ -123,6 +126,7 @@ async function queryLang(user) {
         });
     }
 
-const val = queryLang("newUser");
+const wallet = "829e268ae2d80f930ece00cc07786860021ee733baf5ebda83f0924e6022276b";
+const val = queryLang(wallet);
 console.log("Returned:", val);
 
