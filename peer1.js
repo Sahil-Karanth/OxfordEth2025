35c8f86e866ae395df496a4908661783cc98c38c
Andrei Lazarescu <avl24@ic.ac.uk>
2025-02-08 19:48:37 +0000

    Redis catch on each peer

--- peer1.js
+++ peer1.js
@@ -3,6 +3,7 @@ const Gun = require('gun');
 const gun = Gun({
     peers: [], // Other peers will connect to this
     file: 'data1.json', // Storage file
+    redis: { host: '127.0.0.1', port: 6379 },
     web: require('http').createServer().listen(3001) // Start server
 });
 
--- peer2.js
+++ peer2.js
@@ -3,6 +3,7 @@ const Gun = require('gun');
 const gun = Gun({
     peers: ['http://localhost:3001/gun'], // Connect to Peer 1
     file: 'data2.json',
+    redis: { host: '127.0.0.1', port: 6379 },
     web: require('http').createServer().listen(3002)
 });
 
