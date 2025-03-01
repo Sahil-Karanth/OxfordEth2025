cd0ddff2e3ae6df5456f9d6f1c47dd022436d6d1
Sahil-Karanth <1paleking1@gmail.com>
2025-03-01 01:37:51 +0000

    removed unused files

--- frontend/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-// Expose a safe API to the renderer process
-contextBridge.exposeInMainWorld('electronAPI', {
-  receiveArray: (callback) => ipcRenderer.on('array-data', callback),
-  requestArray: () => ipcRenderer.send('request-array'),
-});
